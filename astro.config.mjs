0fbb3c6788a97d0725f618eca552ece5ca809d6f
unknown <muhammaddiohamanrora@gmail.com>
2026-01-06 21:01:03 +0700

    Add sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  site: "https://toolzya.com",
+
+  integrations: [
+    sitemap(),
+  ],
   output: "server",
   adapter: cloudflare(),
 });
